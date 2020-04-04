0cca19ec993540a351651ea9b5b9fc4743de5f1f
bayanavv <bayanav.sergen@icloud.com>
2020-04-04 22:38:20 +0300

    update

--- Game-Dev-Idea-2/Assets/Scripts/Player.cs
+++ Game-Dev-Idea-2/Assets/Scripts/Player.cs
@@ -12,9 +12,13 @@ public class Player : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
-   void FixedUpdate ()
+   void FixedUpdate ()//running on a fixed timer
     {
+        /*moving left and right and all ways when doing movment multiply with Time.fixedDeltaTime becouse we insie
+         od fixedUpdate with is the amount of time passed since our last fixed update was called and there for
+         it will make it independet on whatever are fixedDeltaTime  its running and then we will have a speed veriable*/
         float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+        
 
         Vector2 newPosition = rb.position + Vector2.right * x;
 
