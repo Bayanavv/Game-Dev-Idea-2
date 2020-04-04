a3c5e144633c8b5ebde612164761b39f5bd1374f
bayanavv <bayanav.sergen@icloud.com>
2020-04-04 14:26:41 +0300

    Create BlockSponer.cs

---
+++ Game-Dev-Idea-2/Assets/Scripts/BlockSponer.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+
+public class BlockSponer : MonoBehaviour
+{
+
+    public Transform[] spwnPoints;
+
+    public GameObject blockPrefab;
+
+    private float timeToSpawn = 2f;
+
+    public float timeBetweenWaves = 1f;
+
+   
+    void Start()
+    {
+       
+    }
+    void Update()
+    {
+        if(Time.time >= timeToSpawn)
+        {
+            SpawnBlocks();
+            timeToSpawn = Time.time + timeBetweenWaves;
+        }
+        
+    }
+    void SpawnBlocks()
+    {
+        int randomIndex = Random.Range(0, spwnPoints.Length);
+
+        for (int i = 0; i < spwnPoints.Length; i++)
+        {
+            if (randomIndex != i)
+            {
+                Instantiate(blockPrefab, spwnPoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+
+} 
