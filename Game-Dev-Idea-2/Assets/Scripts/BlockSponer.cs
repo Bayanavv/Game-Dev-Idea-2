858bcc42c5352e38e4422cd3b0d39bcfdd6f925d
bayanavv <bayanav.sergen@icloud.com>
2020-04-05 23:27:22 +0300

    update

--- Game-Dev-Idea-2/Assets/Scripts/BlockSponer.cs
+++ Game-Dev-Idea-2/Assets/Scripts/BlockSponer.cs
@@ -30,13 +30,22 @@ public class BlockSponer : MonoBehaviour
     void SpawnBlocks()
     {   
         /*selecting a random spawn point
+         this unity way of getting a random number betwen zero of the array to the array length# 
          */
         int randomIndex = Random.Range(0, spwnPoints.Length);
 
+        /*looping threw all the spawn points*/
         for (int i = 0; i < spwnPoints.Length; i++)
         {
+            /*do thonting X amount of times
+             
+             loop prooper
+             
+             so if this time that we are looping over this index that we reach this spawn point should be the one selected in randomIndex
+             if its the one that we chose the random single one that we dont want to spawn a block if that the case well then we dont want to do anything*/
             if (randomIndex != i)
             {
+                /*here its not equal to that then we want to spawn in a block*/
                 Instantiate(blockPrefab, spwnPoints[i].position, Quaternion.identity);
             }
         }
