2d29d42c7ba7c82713ab1a55a03c380b61a30046
bayanavv <bayanav.sergen@icloud.com>
2020-04-05 21:50:14 +0300

    comments

--- Game-Dev-Idea-2/Assets/Scripts/Player.cs
+++ Game-Dev-Idea-2/Assets/Scripts/Player.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 15f;
-
     private Rigidbody2D rb;
     public float mapWidth = 5f;
 
@@ -19,7 +18,10 @@ public class Player : MonoBehaviour
          it will make it independet on whatever are fixedDeltaTime  its running and then we will have a speed veriable*/
         float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
         
-
+        /*vector2 2 demensial that cheking for colisions and moving to a new position 
+         usfull thing is the position that we want to move to is our current position plus some
+         x value so we want to move to our current position either bit to the right or a bit to 
+         left our current position is called rb.position*/
         Vector2 newPosition = rb.position + Vector2.right * x;
 
         newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
