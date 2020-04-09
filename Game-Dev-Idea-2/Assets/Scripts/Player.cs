f8746b7b3b390e00fba3aa0038a520cf94bc048d
bayanavv <bayanav.sergen@icloud.com>
2020-04-09 19:47:49 +0300

    Update Player.cs

--- Game-Dev-Idea-2/Assets/Scripts/Player.cs
+++ Game-Dev-Idea-2/Assets/Scripts/Player.cs
@@ -20,23 +20,25 @@ public class Player : MonoBehaviour
     void Update()
     {
         PlayerHealth();
+        healthDisplay.text = playerHealth.ToString();
+
     }
    void FixedUpdate ()//running on a fixed timer
     {
         /*moving left and right and all ways when doing movment multiply with Time.fixedDeltaTime becouse we insie
-         od fixedUpdate with is the amount of time passed since our last fixed update was called and there for
-         it will make it independet on whatever are fixedDeltaTime  its running and then we will have a speed veriable*/
+         *od fixedUpdate with is the amount of time passed since our last fixed update was called and there for
+         *it will make it independet on whatever are fixedDeltaTime  its running and then we will have a speed veriable*/
         float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
         
         /*vector2 2 demensial that cheking for colisions and moving to a new position 
-         usfull thing is the position that we want to move to is our current position plus some
-         x value so we want to move to our current position either bit to the right or a bit to 
-         left our current position is called rb.position*/
+         *usfull thing is the position that we want to move to is our current position plus some
+         *x value so we want to move to our current position either bit to the right or a bit to 
+         *left our current position is called rb.position*/
         Vector2 newPosition = rb.position + Vector2.right * x;
 
         /*clamping the value of the width of the map or other world limiting the value between so
-         it doesnt go above the width ussing Mathf.Clamp with take in a value that it should limit 
-         then a minum vakue and a maximum value */
+         *it doesnt go above the width ussing Mathf.Clamp with take in a value that it should limit 
+         *then a minum vakue and a maximum value */
         newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
 
         /* RigitBody2D.MovePosition is basicaly moving the object to certain position and cheking for
@@ -49,18 +51,18 @@ public class Player : MonoBehaviour
     our 2D Colider */
     void OnCollisionEnter2D ()
     {
-        /*FindObjectType allow us to call functions from enother scripts*/
-        //FindObjectOfType<GameManager>().EndGame();
+        
     }
 
     public void PlayerHealth ()
     {
         if(playerHealth <= 0)
         {
-           // Destroy(gameObject);
+           Destroy(gameObject);
+           FindObjectOfType<GameManager>().EndGame();
         }
 
-        healthDisplay.text = playerHealth.ToString();
-        //healthDisplay.text = playerHealth.ToString();
+        
+        
     }
 }
